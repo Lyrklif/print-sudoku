192d9a2ef667ccaecb3ae815f67fe80f830feecd
Lyrklif <kochurow121@gmail.com>
2020-04-18 20:48:53 +0400

    исправление массива судоку

--- src/functions/getSudokuArray.js
+++ src/functions/getSudokuArray.js
@@ -7,10 +7,19 @@ import { makepuzzle, solvepuzzle } from "sudoku";
  * @returns {object []}
  */
 const getSudokuArray = () => {
-  var puzzle = makepuzzle();
-  var solution = solvepuzzle(puzzle);
+  let puzzle = makepuzzle(); // судоку с отсутствием части цифр
+  let solution = solvepuzzle(puzzle); // решение
 
-  return solution;
+  // исправленный массив. все числа увеличены на 1, 
+  // т.к. в оригинальном массиве идут числа от 0 до 8
+  let fixedPuzzle = []; 
+
+  // увеличить все числа на 1
+  solution.forEach((number, i) => {
+    fixedPuzzle[i] = ++number;
+  });
+
+  return fixedPuzzle;
 };
 
 export default getSudokuArray;
