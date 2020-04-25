337936f4a8013b36c30ec51fd83ce8d548a4048a
Lyrklif <kochurow121@gmail.com>
2020-04-25 17:58:39 +0400

    4rqekj footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,20 @@
+
+import React from "react";
+
+
+// подвал
+const Footer = () => {
+
+  return (
+    <footer class="no-print hide">
+      <p>
+        Icons made by
+          <a href="https://www.flaticon.com/authors/freepik" title="Freepik"> Freepik </a>
+          from
+          <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+      </p>
+    </footer>
+  )
+};
+
+export default Footer;
